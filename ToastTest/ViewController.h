c0637dc3438806f64ace45979bb36c10a8138b43
Musk66 <1592784045@qq.com>
2019-11-13 11:01:30 +0800

    Initial Commit

---
+++ ToastTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToastTest
+//
+//  Created by tiger on 2018/10/17.
+//  Copyright © 2018 tiger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ToastTest/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  ToastTest
+//
+//  Created by tiger on 2018/10/17.
+//  Copyright © 2018 tiger. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "UIView+Toast.h"
+
+@interface ViewController ()
+
+- (IBAction)alertBtnClick:(UIButton *)sender;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (IBAction)alertBtnClick:(UIButton *)sender {
+    CSToastStyle *toastStyle = [[CSToastStyle alloc] initWithDefaultStyle];
+    toastStyle.backgroundColor = [UIColor brownColor];
+    toastStyle.titleColor = [UIColor whiteColor];
+    [self.view makeToast:@"top" duration:2.0f position:CSToastPositionTop style:toastStyle];
+    [self.view makeToast:@"center" duration:2.0f position:CSToastPositionCenter style:toastStyle];
+    [self.view makeToast:@"bottom" duration:2.0f position:CSToastPositionBottom style:toastStyle];
+}
+
+@end
